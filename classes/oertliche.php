20c3fd6a26cbbe6e77d9029d64614cee8be48d1f
Roland Greim <roland@stempelgreim.de>
2021-06-11 01:32:28 +0200

    Fix regex for oertliche

--- classes/oertliche.php
+++ classes/oertliche.php
@@ -1,46 +1,50 @@
 <?php
-defined( 'INC' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
-require_once( 'provider.php' );
+defined('INC') or die(header('HTTP/1.0 403 Forbidden'));
+require_once('provider.php');
 
-class oertliche extends provider {
-    public function query( $params ) {
+class oertliche extends provider
+{
+    public function query($params)
+    {
         $phone_number = 0;
-        if ( isset( $params['hm'] ) & $params['hm'] != '*' ) {
+        if (isset($params['hm']) & $params['hm'] != '*') {
             $phone_number = $params['hm'];
-            $this->normalizePhoneNumber( $phone_number );
+            $this->normalizePhoneNumber($phone_number);
 
             $url = "http://www.dasoertliche.de/Controller?form_name=search_inv&ph=$phone_number";
         } else {
             $urlParams = "";
-            foreach (array('fn' => 'fn','ln' => 'kw','ct' => 'ci','st' => 'st') as $key => $val) {
-                if (isset( $params[$key] ) & strlen($params[$key]) > 2) 
-                    $urlParams .= '&'.$val.'='.$params[$key];
+            foreach (array('fn' => 'fn', 'ln' => 'kw', 'ct' => 'ci', 'st' => 'st') as $key => $val) {
+                if (isset($params[$key]) & strlen($params[$key]) > 2)
+                    $urlParams .= '&' . $val . '=' . $params[$key];
             }
             $url = "https://www.dasoertliche.de/?form_name=search_nat_ext$urlParams";
         }
-        $matches = $this->QueryDasOertlicheDe( $url );
-        if ( $matches != NULL ) {
-            foreach ( $matches AS $match ) {
+        $matches = $this->QueryDasOertlicheDe($url);
+        if ($matches != NULL) {
+            foreach ($matches as $match) {
                 $contact = new contact();
-                $contact->setUrl( $match[4] );
-                $contact->setCity( $match[5] );
-                $contact->setZipcode( $match[9] );
-                $contact->setStreet( $match[10] );
-                $contact->setHouseNumber( $match[11] );
-                $contact->setLastName( explode( ' ', $match[14], 2 )[0] );
-                $contact->setFirstName( explode( ' ', $match[14], 2 )[1] );
-                $contact->setHomeNumber( $phone_number );
-                $this->addContact( $contact );
+                $contact->setUrl($match[4]);
+                $contact->setCity($match[5]);
+                $contact->setZipcode($match[9]);
+                $contact->setStreet($match[10]);
+                $contact->setHouseNumber($match[11]);
+                $contact->setLastName(explode(' ', $match[14], 2)[0]);
+                $contact->setFirstName(explode(' ', $match[14], 2)[1]);
+                $contact->setHomeNumber($phone_number);
+                $this->addContact($contact);
             }
         }
     }
 
-    private function QueryDasOertlicheDe( $url ) {
-        $data = file_get_contents( $url );
-        preg_match( '/var\shandlerData\s=\s*(?P<v>(?>\[(?&v)?(?>\,\s*(?&v))*\])|(?>\'[^\']*\' ) );
-        /mx', $data, $entry );
-        return json_decode(str_replace('\'', '"', $entry[1] ) );
+    private function QueryDasOertlicheDe($url)
+    {
+        $data = file_get_contents($url);
+        preg_match(
+            '/var\shandlerData\s=\s*(?P<v>(?>\[(?&v)?(?>\,\s*(?&v))*\])|(?>\"[^\"]*\")|(?>null));/mx',
+            $data,
+            $entry
+        );
+        return json_decode($entry[1]);
     }
 }
-
-?>
